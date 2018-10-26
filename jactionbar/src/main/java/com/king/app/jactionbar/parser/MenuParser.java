761afcb34d757ff013007bbbe07c07596a9dada7
JingYang <jingyang@tcsl.com.cn>
2018-10-26 09:18:11 +0800

    v1.0.7 fix conflict attribute with android system(occurred in com.android.tools.build:gradle 3.2.1)

--- jactionbar/src/main/java/com/king/app/jactionbar/parser/MenuParser.java
+++ jactionbar/src/main/java/com/king/app/jactionbar/parser/MenuParser.java
@@ -122,7 +122,7 @@ public class MenuParser {
         int id = a.getResourceId(R.styleable.JMenuItem_android_id, -1);
         int iconRes = a.getResourceId(R.styleable.JMenuItem_android_icon, -1);
         String title = a.getText(R.styleable.JMenuItem_android_title).toString();
-        int showAsAction = a.getInt(R.styleable.JMenuItem_showAsAction, -1);
+        int showAsAction = a.getInt(R.styleable.JMenuItem_android_showAsAction, -1);
 
         JMenuItem item = new JMenuItem();
         item.setId(id);
